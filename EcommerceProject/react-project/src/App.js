ae73a08d873b391c04f925315702afbf0687d4a5
rupesh <rupeshspund@gmail.com>
2020-06-24 00:28:18 +0530

    Added BASIC-structured APP

--- EcommerceProject/react-project/src/App.js
+++ EcommerceProject/react-project/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details.js";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/Cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;

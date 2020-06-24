be4acb0a1f1fc5864666c4f6cefd8fd2b41cfa2e
sachin <sachinkraj2941@gmail.com>
2020-06-24 19:18:33 +0530

    Added Product.js

--- Product.js
+++ Product.js
@@ -8,33 +8,33 @@ export default class Product extends Component {
         const {id, title, img, price, inCart}= this.props.Product;
         return (
             
-        <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3" >
-            <div>
-                <div className="img-container p-5 " onClick={() => console.log( 'you clicked me on image container');}>
-                    <link to "/details">
-                        <img src={img}alt ="product" className ="card-img-top"/>
-                    </link>
-                    <button className="cart-btn" disabled={inCart ? true : false} onClick = {() =>{
-                    console.log("Added to the cart")} }>   
-                         {inCart ? (<p className ="text-capitalize mb-0" disabled/>)
-                          In Cart<p>:(<i className="fas fa-cart-plus"/>) }         
-                    </button> 
-                </div>
-                {/*Card footer */}
-                <div className ="card-footer d-flex justify-content-beetween">
-                <p className = "align-self-center mb-0">
-                {title}
-                </p>
-                <h5 className="text-blue font-italic mb-0">
-                    <spam className="mr-1">$</spam>
-                    {price}
-                </h5>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3" >
+                <div>
+                    <div className="img-container p-5 " onClick={() => console.log( 'you clicked me on image container');}>
+                        <link to "/details">
+                            <img src={img}alt ="product" className ="card-img-top"/>
+                        </link>
+                        <button className="cart-btn" disabled={inCart ? true : false} onClick = {() =>{
+                        console.log("Added to the cart")} }>   
+                             {inCart ? (<p className ="text-capitalize mb-0" disabled/>)
+                            In Cart<p>:(<i className="fas fa-cart-plus"/>) }         
+                        </button> 
+                    </div>
+                    {/*Card footer */}
+                    <div className ="card-footer d-flex justify-content-beetween">
+                    <p className = "align-self-center mb-0">
+                    {title}
+                    </p>
+                    <h5 className="text-blue font-italic mb-0">
+                        <spam className="mr-1">$</spam>
+                        {price}
+                    </h5>
 
+                    </div>
                 </div>
-            </div>
-        </ProductWrapper>
+            </ProductWrapper>
                 
-            );
+        );
     }
 }
 

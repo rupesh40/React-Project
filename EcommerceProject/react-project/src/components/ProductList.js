eb5be2ea99787cb9fb2e63540c1e65f53763a975
rupesh <rupeshspund@gmail.com>
2020-06-24 18:14:31 +0530

    updated the components as per task 2

--- EcommerceProject/react-project/src/components/ProductList.js
+++ EcommerceProject/react-project/src/components/ProductList.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "./Title"; 
+import {storeProducts} from "../data"
 
 export default class ProductList extends Component {
+  state= {
+    products:storeProducts
+  }
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <React.Fragment>
+        <div className ="py-5">
+ <            div className = "container">
+                  <Title name="our" title="product"/>
+                  <div className ="row">
+  
+                  </div> 
+            </div>
+        </div>        
+      </React.Fragment>
     );
   }
 }

5269596e0f5dee2256b249f37a593899510634c6
rupesh <rupeshspund@gmail.com>
2020-06-24 23:18:23 +0530

    updated the th react app

--- EcommerceProject/react-project/src/components/Product.js
+++ EcommerceProject/react-project/src/components/Product.js
@@ -1,46 +1,42 @@
 import React, { Component } from 'react'
-import styled from "styled-Components";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import{ProductConsumer} from "../context";
 
 export default class Product extends Component {
-    render() {
-        const {id, title, img, price, inCart}= this.props.product;
-        return (
-            
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3" >
-              <div className="card">
-                <div>
-                    <div className="img-container p-5 " onClick={() => console.log( 'you clicked me on image container')}>
-                        <Link to="/details">
-                            <img src={img} alt ="product" className ="card-img-top"/>
-                        </Link>
-                        <button className="cart-btn" disabled={inCart ? true : false} onClick = {() =>{
-                        console.log("Added to the cart")} }>   
-                             {inCart ? (<p className ="text-capitalize mb-0" disabled>
-                             {" "}
-                            in Cart </p>):(<i className="fas fa-cart-plus"/>) }         
-                        </button> 
-                    </div>
-                    {/*Card footer */}
-                    <div className ="card-footer d-flex justify-content-between">
-                    <p className = "align-self-center mb-0">
-                    {title}
-                    </p>
-                    <h5 className="text-blue font-italic mb-0">
-                        <span className="mr-1">$</span>
-                        {price}
-                    </h5>
+  render() {
+      const {id, title, img, price, inCart}= this.props.product;
+      return (
+          
+          <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3" >
+              <div>
+                  <div className="img-container p-5 " onClick={() => console.log( 'you clicked me on image container')}>
+                      <Link to ="/details">
+                          <img src={img}alt ="product" className ="card-img-top"/>
+                      </Link>
+                      <button className="cart-btn" disabled={inCart ? true : false} onClick = {() =>{
+                      console.log("Added to the cart")} }>   
+                           {inCart ? (<p className ="text-capitalize mb-0" disabled>
+                          In Cart</p>):(<i className="fas fa-cart-plus"/>) }         
+                      </button> 
+                  </div>
+                  {/*Card footer */}
+                  <div className ="card-footer d-flex justify-content-beetween">
+                  <p className = "align-self-center mb-0">
+                  {title}
+                  </p>
+                  <h5 className="text-blue font-italic mb-0">
+                      <spam className="mr-1">Rs</spam>
+                      {price}
+                  </h5>
 
-                    </div>
-                </div>
-                </div>
-            </ProductWrapper>
-                
-        );
-    }
+                  </div>
+              </div>
+          </ProductWrapper>
+              
+      );
+  }
 }
-
 const ProductWrapper = styled.div `
 .Card{
  border-color:transparent;
